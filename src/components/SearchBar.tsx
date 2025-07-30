fcc09b6c50d1661ae7f823bae91089e0483a4c6a
Marlon Calderón <mrln9eca@gmail.com>
2025-07-30 15:55:39 -0600

    initial commit

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,17 @@
+type Props = {
+    value: string;
+    onChange: (value: string) => void;
+};
+
+function SearchBar({ value, onChange }: Props) {
+    return (
+        <input
+            type="text"
+            placeholder="Buscar por título, número o letra..."
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+        />
+    );
+}
+
+export default SearchBar;
